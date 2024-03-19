43ae3d8f8f50ec8ee3ae9dc46ba7c00e4aceadb7
dev-taimoor <taimoor220297@gmail.com>
2024-03-19 21:02:23 +0500

    cars crud admin dashboard

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,7 +1,70 @@
 class CarsController < ApplicationController
 
+  def index
+    @car = Car.new
+    @cars = Car.all
+  end
+
+  def destroy
+    begin
+      @car = Car.find(params[:id])
+      authorize! :destroy, @car
+
+      if @car.destroy
+        redirect_to cars_path, notice: 'Car was successfully destroyed.'
+      else
+        redirect_to cars_path, notice: 'Car was not created.'
+      end
+    rescue => e
+      redirect_to cars_path, notice: "An error occurred: #{e.message}"
+    end
+  end
+
   def create
-    byebug
+    begin
+      @car = Car.new(car_params)
+      authorize! :create, @car
+      @car.image.attach(params[:image]) if params[:image].present?
+
+      if @car.save
+        redirect_to cars_path, notice: 'Car was successfully created.'
+      else
+        redirect_to cars_path, notice: 'Car was not created.'
+      end
+    rescue => e
+      redirect_to cars_path, notice: "An error occurred: #{e.message}"
+    end
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def edit
+    @car = Car.find(params[:id])
+  end
+
+  def update
+    begin
+      @car = Car.find(params[:id])
+      authorize! :update, @car
+
+      @car.image.attach(params[:image]) if params[:image].present?
+
+      if @car.update(car_params)
+        redirect_to cars_path, notice: 'Car was successfully updated.'
+      else
+        redirect_to cars_path, notice: 'Car was not updated.'
+      end
+    rescue => e
+      redirect_to cars_path, notice: "An error occurred: #{e.message}"
+    end
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:make_model, :reserve_auction_price, :buy_now_price, :description, :salvage_category_id, :category_id, :location, :delivery_cost, :image)
   end
 
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,29 +4,23 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the user here. For example:
-    #
-    #   return unless user.present?
-    #   can :read, :all
-    #   return unless user.admin?
-    #   can :manage, :all
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, published: true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
+    cannot :manage, :all
+
+    if user.role == "admin"
+      set_admin_abilities(user)
+    else 
+      set_user_abilities(user)
+    end
   end
+
+  private
+
+  def set_admin_abilities(user)
+    can :manage, :all
+  end
+
+  def set_user_abilities(user)
+    can :read, Car
+  end
+
 end
